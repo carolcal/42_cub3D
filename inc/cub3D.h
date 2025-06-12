1c46e175fece7111904734641d426a80a37d41f2
carolcal <carolinecaleguer@gmail.com>
2025-06-12 16:41:20 -0300

    creating files

---
+++ inc/cub3D.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 16:30:51 by marvin            #+#    #+#             */
+/*   Updated: 2025/06/12 16:30:51 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <math.h>
+# include <sys/times.h>
+
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 16:30:23 by marvin            #+#    #+#             */
+/*   Updated: 2025/06/12 16:30:23 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "cub3D.h"
+
+void    validate_map(const char *map_file)
+{
+    printf("Validating map: %s\n", map_file);
+}
+
+void    init_game(const char *map_file)
+{
+    printf("Initializing game with map: %s\n", map_file);
+}
+
+int    main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <map_file>\n", argv[0]);
+        return (EXIT_FAILURE);
+    }
+
+    validate_map(argv[1]);
+    init_game(argv[1]);
+
+    return (EXIT_SUCCESS);
+}
